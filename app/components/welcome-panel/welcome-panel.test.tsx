8b5603acb2a2323ef02a5fc8ef1455bc4d2f8a21
Regina Mos <regishechka.m@gmail.com>
2025-09-07 18:02:11 +0500

    feat: update test with i18n

--- app/components/welcome-panel/welcome-panel.test.tsx
+++ app/components/welcome-panel/welcome-panel.test.tsx
@@ -1,12 +1,10 @@
 import { it, expect, describe } from 'vitest';
-
 import { render, screen } from '@testing-library/react';
-
 import WelcomePanel from '@/components/welcome-panel/index';
-
 import '@testing-library/jest-dom/vitest';
-
 import { MemoryRouter } from 'react-router';
+import { I18nextProvider } from 'react-i18next';
+import i18n from '@/i18n';
 
 const paths = ['/sign-in', '/sign-up'];
 
@@ -14,12 +12,16 @@ describe('tests WelcomePanel', () => {
   it('should show user "Welcome", Links: Sign In, Sign Up', () => {
     render(
       <MemoryRouter>
-        <WelcomePanel />
+        <I18nextProvider i18n={i18n}>
+          <WelcomePanel />
+        </I18nextProvider>
       </MemoryRouter>
     );
-    expect(screen.getByText('Welcome!')).toBeInTheDocument();
-    screen
-      .getAllByRole('link')
-      .forEach((link, i) => expect(link).toHaveAttribute('href', paths[i]));
+
+    expect(screen.getByText(i18n.t('mainRoute.welcomePanel.welcome') + '!')).toBeInTheDocument();
+
+    screen.getAllByRole('link').forEach((link, i) =>
+      expect(link).toHaveAttribute('href', paths[i])
+    );
   });
 });
